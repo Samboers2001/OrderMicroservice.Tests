47d838a70300ae5b810860f84f43229f860aecb0
Sam <sam@boers.family>
2024-01-16 12:58:16 +0100

    Get All Orders tested

--- OrderControllerTests.cs
+++ OrderControllerTests.cs
@@ -1,24 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using OrderMicroservice.Data.Interfaces;
 using OrderMircroservice.Controllers;
+using OrderMicroservice.Models;
+using System.Collections.Generic;
 using Xunit;
 
 public class OrderControllerTests
 {
     private readonly OrderController _controller;
+    private readonly Mock<IOrderRepo> _mockRepository;
 
     public OrderControllerTests()
     {
-        _controller = new OrderController();
+        _mockRepository = new Mock<IOrderRepo>();
+        _controller = new OrderController(_mockRepository.Object);
+
+        _mockRepository.Setup(repo => repo.GetAllOrders())
+                       .Returns(GetTestOrders());
+    }
+
+    private List<Order> GetTestOrders()
+    {
+        var orders = new List<Order>
+        {
+            new Order() { /* Initialize properties */ },
+            new Order() { /* Initialize properties */ }
+
+        };
+
+        return orders;
     }
 
     [Fact]
-    public void Get_ReturnsExpectedMessage()
+    public void GetAllOrders_ReturnsAllOrders()
     {
         // Act
-        var result = _controller.Get();
+        var result = _controller.GetAllOrders();
 
         // Assert
-        var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        Assert.Equal("Hello from OrderController! And by the way this works too! And is changed now!", okResult.Value);
+        var actionResult = Assert.IsType<ActionResult<IEnumerable<Order>>>(result);
+        var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+        var returnedOrders = Assert.IsType<List<Order>>(okResult.Value);
+        Assert.Equal(2, returnedOrders.Count); 
     }
+
 }
